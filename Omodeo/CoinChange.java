79352307d83eefcbbe02a78dde6800d6164e1915
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2019-04-12 16:31:56 +0200

    ehm

--- Omodeo/CoinChange.java
+++ Omodeo/CoinChange.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.math.BigDecimal;
 
 public class CoinChange{
   public static void main(String[] args) {
@@ -17,47 +18,70 @@ public class CoinChange{
         else System.out.println(list.toString());
       } else {
         if(!change.insert(d)) System.out.println("Moneta non valida!");
-        else System.out.println(change.get());
+        else System.out.println(change.getTotal());
       }
     }
   }
 
-  //private final int[] coins;
+  private final int[] coins;
   private final double[] coinsValue;
-  private double input;
+  private BigDecimal input = new BigDecimal(0);
 
   public CoinChange(double[] coinsValue) {
     this.coinsValue = coinsValue;
+    this.coins = new int[]{3,3,3,3,3,3,3,3};
+  }
+
+  public CoinChange(int[] startCoins, double[] coinsValue) {
+    this.coinsValue = coinsValue;
+    this.coins = startCoins;
+  }
+
+  public int take(BigDecimal total, int[] coins, int index) {
+    int temp = index;
+    int spot = -1;
+    for(int j = 0; j < coins.length; j++) {
+      if(temp - coins[j] < 0) {
+        spot = j;
+        break;
+      }
+      temp -= coins[j];
+    }
+
+    System.out.println(String.format("spot: %d, index: %d, total: %f", spot, index, total.doubleValue()));
+
+    if(spot == -1) return -1;
+    index++;
+
+    BigDecimal result = total.subtract(new BigDecimal(coinsValue[spot]));
+    System.out.println(String.format("result: %f", result.doubleValue()));
+    if(result.doubleValue() == 0) return 1;
+    else if(result.doubleValue() < 0) {
+      return take(total,coins,index);
+    } else {
+      int dontTake = take(total,coins,index);
+      int take = take(result,coins,index);
+
+      if(dontTake == -1 && take == -1) return -1;
+      else if(dontTake == -1 && take != -1) return take + 1;
+      else if(dontTake != -1 && take == -1) return dontTake;
+      else {
+        if(dontTake < take) return dontTake;
+        else return take + 1;
+      }
+    }
   }
 
   public List<Double> clear() {
-    //int[] tempCoins = deepCopy(this.coins);
-    double tempInput = this.input;
-
-    List<Double> ls = new ArrayList<>();
-    for(int i = coinsValue.length - 1; tempInput > 0 && i >= 0; i--) {
-      while(tempInput >= this.coinsValue[i] /*&& tempCoins[i] > 0*/) {
-        ls.add(coinsValue[i]);
-        tempInput -= coinsValue[i];
-        //tempCoins[i]--;
-      }
-    }
-
-    // impossibile
-    if(tempInput > 0) return null;
-    else {
-      this.input = tempInput;
-      //this.coins = tempCoins;
-
-      return ls;
-    }
+    System.out.println(take(this.input,this.coins,0));
+    return null;
   }
 
   public boolean insert(double coin) {
     for(int i = 0; i < coinsValue.length; i++) {
       if(coinsValue[i] == coin) {
-        this.input += coin;
-        //this.coins[i]++;
+        this.input = this.input.add(new BigDecimal(coin));
+        this.coins[i]++;
         return true;
       }
     }
@@ -65,8 +89,8 @@ public class CoinChange{
     return false;
   }
 
-  public double get() {
-    return this.input;
+  public double getTotal() {
+    return this.input.doubleValue();
   }
 
   public static int[] deepCopy(int[] i) {
